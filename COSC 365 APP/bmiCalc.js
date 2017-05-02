74393f86c06e2116c5cf2f54b04055e0f1b2eb71
jabbman <qzhs@iup.edu>
2017-05-02 15:27:26 -0400

    Add files via upload

---
+++ COSC 365 APP/bmiCalc.js
@@ -0,0 +1,27 @@
+function computeBMI() {
+            // user inputs
+            var height = Number(document.getElementById("height").value);
+            var weight = Number(document.getElementById("weight").value);
+
+
+            //Convert all units to metric
+            height /= 39.3700787;
+            weight /= 2.20462;
+
+            //Perform calculation
+            var BMI = weight / Math.pow(height, 2);
+
+            //Display result of calculation
+            document.getElementById("output").innerText = Math.round(BMI * 100) / 100;
+
+            var output = Math.round(BMI * 100) / 100
+            if (output < 18.5)
+                document.getElementById("comment").innerText = "Underweight";
+            else if (output >= 18.5 && output <= 25)
+                document.getElementById("comment").innerText = "Normal";
+            else if (output >= 25 && output <= 30)
+                document.getElementById("comment").innerText = "Overweight";
+            else if (output > 30)
+                document.getElementById("comment").innerText = "Obese";
+             
+        }
